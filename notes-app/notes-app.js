f417f60664023a12bcb82b1fff6564c377ed58c6
rakesh050791 <rakesh050791@gmail.com>
2022-06-21 23:34:22 +0200

    Introduction to DOM, document object model

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,1 +1,19 @@
-console.log("I am inside notes-app.js file")
+// DOM - Document object model
+
+
+
+// Query & remove
+// const p = document.querySelector('p')
+
+// p.remove()
+
+
+// Query all & remove 
+const ps = document.querySelectorAll('p')
+ps.forEach(function(p) {
+    // p.remove()
+    // console.log(p.textContent)
+    p.textContent += '**'
+    console.log(p.textContent)
+})
+
